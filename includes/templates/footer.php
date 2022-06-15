da756a7097b048b498a7160393487540418e36d1
Abdulrahman <>
2022-06-16 00:04:14 +0200

    add popup chat

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -89,5 +89,18 @@
 		<script src="<?php echo $js ?>bootstrap.min.js"></script>
 		<script src="<?php echo $js ?>all.min.js"></script>
 		<script src="<?php echo $js ?>main.js"></script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/62aa1d7d7b967b117994bc8e/1g5k86dal';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+    </script>
+<!--End of Tawk.to Script-->
 	</body>
 </html>
